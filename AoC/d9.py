4726e5d7d92d59f214748f4f036155fd25490d1b
Henning <henning.schei@gmail.com>
2016-12-09 15:11:16 +0100

    Day 9 AoC

---
+++ AoC/d9.py
@@ -0,0 +1,18 @@
+class Person:
+    def __init__(self,name_str):
+        self.name = name_str
+        self.money = 0
+    def send(amount):
+        self.money = self.money - amount
+
+    def recieve(amount):
+        self.money = self.money + amount
+
+    
+# for all lines
+    # append all users in a list
+# make the list unique
+# make an object for each user stating with 0 money
+# for each transaction:
+    p1.send(amount)
+    p2.recieve(amount)
